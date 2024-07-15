61d319dbcc469c7431bb925a442c67be2f6f3c27
celso269 <84720206+celso269@users.noreply.github.com>
2024-07-15 10:47:02 -0300

    Add files via upload

---
+++ calculadora.py
@@ -0,0 +1,13 @@
+a = 100
+b = 20
+
+soma = a + b 
+sub = a - b
+div = a / b
+mul = a * b
+
+print("Resultado da soma:" + str(soma))
+print("Resultado da subtração:" + str(sub))
+print("Resultado da divisão:" + str(div))
+print("Resultado da multiplicação:" + str(mul))
+
